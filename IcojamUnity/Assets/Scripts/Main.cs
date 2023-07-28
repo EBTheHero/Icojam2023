1fff593954f0d183d6339659e57d1d54b35c9a6d
etiennebrisson@hotmail.com <etiennebrisson@hotmail.com>
2023-07-28 12:01:29 -0400

    Main references tilemap

--- IcojamUnity/Assets/Scripts/Main.cs
+++ IcojamUnity/Assets/Scripts/Main.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Main : MonoBehaviour
 {
     public static Main Instance;
+    public Tilemap MainTilemap;
     // Start is called before the first frame update
     void Start()
     {
