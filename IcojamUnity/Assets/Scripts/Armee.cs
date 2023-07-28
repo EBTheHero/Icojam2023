bf1eb1e03a95b4219e432068ea7cd8e308502350
Raph <Raph@DESKTOP-LR6C39T.local>
2023-07-28 12:25:12 -0400

    Update Armee.cs

--- IcojamUnity/Assets/Scripts/Armee.cs
+++ IcojamUnity/Assets/Scripts/Armee.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Armee : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public const float DUREE_DEPLACEMENT = 1f;
+    public static readonly byte[] DE = new byte[6] { 0, 2, 3, 4, 5, 10 };
+
+    [SerializeField] private byte nbDes = 0;
+
+    public bool EnDeplacement { get; private set; } = false;
+    private float t = 0f;
+    private Vector2 posDepart = new Vector2();
+    private Vector2 destination = new Vector2();
+
+    public bool Combattre(byte scoreABattre)
     {
-        
+        byte score = 0;
+        for (byte i = 0; i < nbDes; ++nbDes)
+            score += DE[Random.Range(0, 5)];
+        return score >= scoreABattre;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void InitierDeplacement(Vector2 dest)
     {
-        
+        EnDeplacement = true;
+        t = 0;
+        posDepart = transform.position;
+        destination = dest;
+    }
+
+    private void Update()
+    {
+        if(EnDeplacement)
+        {
+            t += Time.deltaTime;
+            transform.position = Vector2.Lerp(posDepart, destination, t / DUREE_DEPLACEMENT);
+            if (t >= DUREE_DEPLACEMENT)
+            {
+                transform.position = destination;
+                EnDeplacement = false;
+            }
+        }
     }
 }
