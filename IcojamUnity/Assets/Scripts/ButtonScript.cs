a040643d1e18b475b4fe9cf69f48d948e5f01213
Raph <Raph@DESKTOP-LR6C39T.local>
2023-07-30 14:20:04 -0400

    HitboxesGrossies

--- IcojamUnity/Assets/Scripts/ButtonScript.cs
+++ IcojamUnity/Assets/Scripts/ButtonScript.cs
@@ -6,6 +6,17 @@ public class ButtonScript : MonoBehaviour
 {
     private void OnMouseOver()
     {
+        print("Hey!!!");
         SoundManager.Play("glitch_004");
+    }
+
+    private void OnMouseEnter()
+    {
+        print("Ho!!!");
+    }
+
+    private void OnMouseDown()
+    {
+        SoundManager.Play("confirmation_003");
     }
 }
