7886bc6d0126f9c3ac04252bd8d945e5dc2d1be6
etiennebrisson@hotmail.com <etiennebrisson@hotmail.com>
2023-07-30 20:23:16 -0400

    Ah yes, calling Application close on the first frame of the game. Yes.

--- IcojamUnity/Assets/Scripts/ExitGame.cs
+++ IcojamUnity/Assets/Scripts/ExitGame.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ExitGame : MonoBehaviour
@@ -7,13 +5,7 @@ public class ExitGame : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        QuitGame();
     }
 
     public void QuitGame()
