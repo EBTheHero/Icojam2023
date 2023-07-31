47c03436b9fefc106872007bb0746ef23fcec4a3
etiennebrisson@hotmail.com <etiennebrisson@hotmail.com>
2023-07-30 21:15:45 -0400

    I am into BDSM and Unity is my dom

--- IcojamUnity/Assets/Scripts/GameManager.cs
+++ IcojamUnity/Assets/Scripts/GameManager.cs
@@ -1,37 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+	[NonSerialized] public bool DevMode = false;
+
+
+	public static GameManager Instance = null;
+
+	private void Awake()
+	{
 #if UNITY_EDITOR
-    public bool DevMode = true;
-#else
-    public bool DevMode = false;
+		DevMode = true;
+
 #endif
-    
-    public static GameManager Instance = null;
 
-    private void Awake()
-    {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(this);
-        }
-        else
-            Destroy(gameObject);
-    }
+		if (Instance == null)
+		{
+			Instance = this;
+			DontDestroyOnLoad(this);
+		}
+		else
+			Destroy(gameObject);
+	}
 
-    public static void BackToMainMenu()
-    {
-        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().buildIndex);
-        SceneManager.LoadScene("Menu");
-    }
+	public static void BackToMainMenu()
+	{
+		SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+		SceneManager.LoadScene("Menu");
+	}
 
-    public void SetDevMode(bool activated)
-    {
-        DevMode = activated;
-    }
+	public void SetDevMode(bool activated)
+	{
+		DevMode = activated;
+	}
 }
